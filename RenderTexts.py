ee0c49d9de7e5c0946263247433f8a668fefa825
jules3182 <jtoad23@gmail.com>
2023-10-15 19:15:50 -0400

    Began to add in NTFY functionality, still testing and messing aroud with variable updating and refreshing the panel

--- RenderTexts.py
+++ RenderTexts.py
@@ -3,7 +3,7 @@ bl_info = {
     "author" : "Julian Baker (Baked.blend)",
     "description" : "Sends updates to your phone about render progressc via NTFY",
     "blender" : (3, 60, 2),
-    "version" : (0, 5, 5),
+    "version" : (0, 6, 0),
     "location" : "",
     "warning" : "",
     "category" : "Render"
@@ -11,23 +11,6 @@ bl_info = {
 
 import bpy
 import requests
-
-#def render_complete_handler(scene):
-# This function will be called when a render completes
-#   print("complete")
-# Settings panel Class      
-
-def update_notification_option(self, context):
-    # Update the notification option based on the chosen value
-    if context.scene.notification_option == "25":
-        if comp_perc == 25 or 50 or 75 or 100:
-            notify()
-    elif context.scene.notification_option == "50":
-        if comp_perc == 50 or 100:
-            notify()
-    else:
-        if comp_perc == 100:
-            notify()
 
 class SettingsPanel(bpy.types.Panel):
     bl_label = "Render Text Settings"
@@ -58,7 +41,26 @@ class SettingsPanel(bpy.types.Panel):
         else:
             layout.label(text="Notifications Disabled")
 
-            
+          
+def render_complete_handler(scene):
+# This function will be called when a render completes
+    print("Frame complete")
+    if scene.Note_Tog:
+        #SettingsPanel.draw(self, context)
+        # Update the notification option based on the chosen value
+        if scene.notification_option == "25":
+            if comp_perc == 25 or 50 or 75 or 99:
+                mess_out = "Render is now " + str(comp_perc) + "% complete!"
+                notify(mess_out)
+        elif scene.notification_option == "50":
+            if comp_perc == 50 or 99:
+                mess_out = "Render is now " + str(comp_perc) + "% complete!"
+                notify(mess_out)
+        else:
+            if comp_perc == 99:
+                notify("Render complete!")
+                
+  
 # Main Toggle On and Off
 def Note_Tog(self, context):
     scene = context.scene
@@ -80,13 +82,14 @@ bpy.types.Scene.Note_Tog = bpy.props.BoolProperty(
 
     
 # Main Notification Code!!
-def notify():
-    print("filler")
+def notify(message):
+    print(message)
+    #requests.post("https://ntfy.sh/mytopic", data = message)
 
 
 def register():
     # Render Complete Handler
-    #bpy.app.handlers.render_complete.append(render_complete_handler)
+    bpy.app.handlers.render_complete.append(render_complete_handler)
     
     # Settings Panel
     bpy.utils.register_class(SettingsPanel)
@@ -97,8 +100,7 @@ def register():
         items=[("25", "Every 25%", "Notify at 25% progress"),
                ("50", "Every 50%", "Notify at 50% progress"),
                ("100", "Only at the end", "Notify only when rendering is complete")],
-        default="100",
-        update=update_notification_option
+        default="100"
     )
     bpy.types.Scene.notification_interval = bpy.props.FloatProperty(
         name="Notification Interval",
@@ -109,7 +111,7 @@ def register():
     )
     
 def unregister():
-    #bpy.app.handlers.render_complete.remove(render_complete_handler)
+    bpy.app.handlers.render_complete.remove(render_complete_handler)
     bpy.utils.unregister_class(SettingsPanel)
     del bpy.types.Scene.notification_option
     del bpy.types.Scene.notification_interval
